c218cae760ad681dd250184f026138f32902e385
Bernd Eickhoff <beickhoff01@gmail.com>
2016-11-10 13:15:19 +0000

    implement table class

---
+++ test/toy_robots/models/table_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require './toy_robot/models/table'
+
+module ToyRobot
+	module Models
+		class TableTest < Minitest::Test
+			def test_if_dimensions_are_set
+				table = ::ToyRobot::Models::Table.new(999, 100)
+				assert_equal 999, table.send(:width)
+				assert_equal 100, table.send(:height)
+			end
+
+			def test_may_goto?
+				x = rand(10)
+				y = rand(10)
+				table = ::ToyRobot::Models::Table.new x, y
+				refute table.may_goto?(-1, 1)
+				refute table.may_goto?(1, -1)
+
+				refute table.may_goto?(x, 1)
+				refute table.may_goto?(1, y)
+				refute table.may_goto?(100, 100)
+				
+				assert table.may_goto?(0, 0)
+				assert table.may_goto?(x - 1, y - 1)
+			end
+		end
+	end
+end
--- toy_robot/models/table.rb
+++ toy_robot/models/table.rb
@@ -1,12 +1,21 @@
 # Represents the board on which the robot roams
-class Table
-	:private_attr :width, :height
+require './toy_robot/lib/scoped_attr_accessors'
 
-	def initialize width = 5, height = 5
-		# set width and height
-	end
-
-	# return wether position is within bounds
-	def may_goto? x, y
+module ToyRobot
+	module Models
+		class Table
+			private_attr_accessor :width, :height
+		
+			def initialize width = 5, height = 5
+				# set width and height
+				self.width = width
+				self.height = height
+			end
+		
+			# return wether position is within bounds
+			def may_goto? x, y
+				(0...width).include?(x) && (0...height).include?(y)
+			end
+		end
 	end
 end
